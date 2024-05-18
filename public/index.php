1b26f82c7f46ab496e7dca4ef6d1b25c8980c465
LCA Tech <tech@lcalondon.org>
2024-05-18 22:55:17 +0100

    Add Country Codes

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ require 'csrf_token.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Vonage SMS Sender</title>
+    <title>LCA SMS Sender</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
